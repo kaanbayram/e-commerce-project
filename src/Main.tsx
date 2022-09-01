15e4c0b57b8df67dd9373975dfa735485d943352
kaanbayram <kaan.9898@hotmail.com>
2022-09-02 00:07:43 +0300

    [dev] - Getir Assignment Project Startup

---
+++ src/Main.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { locals } from './assets/stylesheets/main.scss'
+
+export class Main extends Component<any, any>
+{
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+                
+            </>
+        );
+    }
+}
